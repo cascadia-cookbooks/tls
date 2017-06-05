309e50af3165dbfc037acf0a6e95374a4b849020
Colby Olson <me@colbyolson.com>
2017-06-05 13:24:49 -0700

    add meta files

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name                'cop_tls'
+description         'Handles TLS related files and actions.'
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+license             'MIT'
+maintainer          'Copious, Inc.'
+maintainer_email    'engineering@copiousinc.com'
+version             '0.0.1'
+source_url          'https://github.com/copious-cookbooks/tls'
+issues_url          'https://github.com/copious-cookbooks/tls/issues'
+
+supports 'ubuntu', '>= 14.04'
+supports 'debian', '>= 6'
+supports 'rhel', '>= 6'
+supports 'centos', '>= 6'
