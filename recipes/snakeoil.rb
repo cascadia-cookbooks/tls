03635e687b6f7e5d8f91ef34e620f738f04de2c8
Colby Olson <me@colbyolson.com>
2017-06-13 15:40:36 -0700

    include more options to v3

--- recipes/snakeoil.rb
+++ recipes/snakeoil.rb
@@ -33,6 +33,9 @@ OU = #{data['section']}
 CN = #{data['hostname']}
 
 [v3_req]
+authorityKeyIdentifier= keyid,issuer
+basicConstraints= CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 
 [alt_names]
