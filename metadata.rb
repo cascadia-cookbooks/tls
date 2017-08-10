842e38d0415ae6289013d9782d6ed0113afbca17
Colby Olson <me@colbyolson.com>
2017-08-10 13:37:39 -0700

    bump to 0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 license             'MIT'
 maintainer          'Copious, Inc.'
 maintainer_email    'engineering@copiousinc.com'
-version             '0.2.1'
+version             '0.2.2'
 source_url          'https://github.com/copious-cookbooks/tls'
 issues_url          'https://github.com/copious-cookbooks/tls/issues'
 
